917ffbcc5c61343ed989d3d04980a864d0aa7bc7
Kathryn Anderson <kat42272@yahoo.com>
2020-10-11 13:19:01 -0700

    Created notifications and linked them

--- notification/views.py
+++ notification/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+
+from .models import NotificationModel
 
 # Create your views here.
-def notification_view(request):
-    return render(request, 'base.html')
+
+@login_required
+def notification_view(request, user_id):
+    notifications = NotificationModel.objects.filter(user_id=user_id)
+    notification_alert = []
+    for notification in notifications:
+        if not notification.time_posted:
+            notification_alert.append(notification.instapost)
+        notification.time_posted = timezone.now()
+        notification.save()
+
+    return render(request, 'base.html', {
+        'notification_alert': notification_alert[::-1]
+    })
