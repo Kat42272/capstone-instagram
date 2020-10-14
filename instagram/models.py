0921c4da87dec283313b912a5370e16e42883346
Albina1993 <alba.tleubergen@gmail.com>
2020-10-14 17:16:27 -0400

    changes in models

--- instagram/models.py
+++ instagram/models.py
@@ -7,17 +7,17 @@ class InstaProfileModel(AbstractUser):
     displayname = models.CharField(max_length=30)
     bio = models.CharField(max_length=255)
     date_created = models.DateTimeField(auto_now_add=True)
-    # url = models.URLField(null=True)
+    url = models.URLField(null=True)
     email = models.EmailField(max_length=254)
     phone = models.CharField(max_length=15)
     following = models.ManyToManyField("self", symmetrical=False)
     picture = models.ImageField(upload_to='static/photo_upload/', blank=True, null=True, verbose_name='picture')
 
-    @property
-    def data_specific(self):
-        date_submit = self.submit_time
-        current_date = datetime.now(timezone.utc)
-        age_day = current_date - date_submit
-        return age_day.year
+    # @property
+    # def data_specific(self):
+    #     date_submit = self.submit_time
+    #     current_date = datetime.now(timezone.utc)
+    #     age_day = current_date - date_submit
+    #     return age_day.year
 
 
--- instapost/urls.py
+++ instapost/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('add_post/', views.add_post_view, name="addpost"),
-    path('likes/<int:likes_id>', views.like_view, name="like_page"),
-    path('dislikes/<int:dislikes_id>', views.dislike_view, name="dislike_page"),
+    path('likes/<int:likes_id>/', views.like_view, name="like_page"),
+    path('dislikes/<int:dislikes_id>/', views.dislike_view, name="dislike_page"),
    
 ]
