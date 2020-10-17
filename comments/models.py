59c0c4cd998e350a3ef096f696765f94f8a013fc
Reginald Tjitradi <rtjitradi@gmail.com>
2020-10-16 20:56:34 -0700

    regenerated db after mod models for comments

--- comments/models.py
+++ comments/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from photogram.models import InstaProfileModel
+from instapost.models import PostModel
 
 
 # Create your models here.
 class CommentModel(models.Model):
-    author = models.ForeignKey(InstaProfileModel, on_delete=models.CASCADE, related_name="author_comments")
-    comments = models.TextField(max_length=140)
+    author_comment = models.ForeignKey(InstaProfileModel, on_delete=models.CASCADE, related_name="author_comment")
+    post_comment = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name="post_comment")
+    comment = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['date_created']
+
     def __str__(self):
-        return self.comments
+        return self.comment
