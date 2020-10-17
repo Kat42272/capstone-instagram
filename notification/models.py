10c93db7eeb13d5f675f75130a87411bff67294a
Reginald Tjitradi <rtjitradi@gmail.com>
2020-10-16 21:27:24 -0700

    pull and compare fr/ alba on commets app

--- notification/models.py
+++ notification/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 from photogram.models import InstaProfileModel
 from instapost.models import PostModel
+from comments.models import CommentModel
+
 
 # Create your models here.
-
 class NotificationModel(models.Model):
-  user = models.ForeignKey(InstaProfileModel, on_delete=models.CASCADE)
-  # instapost = models.ForeignKey(PostModel, on_delete=models.CASCADE)
-  time_posted = models.DateTimeField(default=None, blank=True, null=True)
+    comment_notification = models.ForeignKey(CommentModel, on_delete=models.CASCADE)
+    user_notification = models.ForeignKey(InstaProfileModel, on_delete=models.CASCADE)
+    new_notification = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.comment_notification.comment
