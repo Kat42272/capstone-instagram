16cdd9bb50c4a525048042ed416bc17db923cf84
Reginald Tjitradi <rtjitradi@gmail.com>
2020-10-19 18:17:20 -0700

    modified in instapost views for error handlings

--- instapost/views.py
+++ instapost/views.py
@@ -122,6 +122,8 @@ def delete_post_view(request, post_id):
 #     return render(request, '404.html', status=404)
 
 
+# 500 Internal server error, so from what model do we use objects.get (is it just by manupulating or requesting bad root url while in settings the allowed_hosts are already specified/limited?)
+
 def handler500(request):
     return render(request, '500.html', status=500)
 
